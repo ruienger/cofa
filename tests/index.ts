62498b965cab7af72d9da4127d07d1edb7694b22
ruienger <13643610935@163.com>
2023-06-27 14:55:18 +0800

    [bugfix-test] node:test库仅支持node18+，使用test库代替之

--- tests/index.ts
+++ tests/index.ts
@@ -1,5 +1,5 @@
-import test from "node:test";
-import assert from "node:assert";
+import test from "test";
+import assert from "assert";
 import { generateStandardOutput } from "src/utils";
 
 const rootdir = "test";
