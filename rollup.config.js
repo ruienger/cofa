1a362807fbf16952de386566ecec3bf2fc52f0d0
ruienger <13643610935@163.com>
2023-12-07 14:05:58 +0800

    [feature-config] 更新配置项
    Issue
    
    Resolution
    
    Others
    
    0.0.1版本停止维护

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from "@rollup/plugin-typescript";
 export default {
   input: "src/index.ts",
   output: {
-    file: "index.js",
+    file: "dist/index.js",
     format: "es",
     banner: "#!/usr/bin/env node",
   },
